bda93e1c61f75d83341570fed142ded5139761b3
shudong <wang@126.com>
2017-12-19 15:49:22 +0800

    init

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div id="app">
+    <NavHead/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/app.css'
+import NavHead from '@/components/NavHead'
+export default {
+  name: 'app',
+  components:{
+    NavHead,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/GoodsList.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+      <div class="accessory-result-page accessory-page">
+        <div class="container">
+            <div class="filter-nav">
+                <span class="sortby">Sort by:</span>
+                <a href="javascript:void(0)" class="default cur">Default</a>
+                <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+                <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            </div>
+            <div class="accessory-result">
+                <!-- filter -->
+                <div class="filter stopPop" id="filter">
+                    <dl class="filter-price">
+                        <dt>Price:</dt>
+                        <dd><a href="javascript:void(0)">All</a></dd>
+                        <dd>
+                            <a href="javascript:void(0)">0 - 100</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:void(0)">100 - 500</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:void(0)">500 - 1000</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:void(0)">1000 - 2000</a>
+                        </dd>
+                    </dl>
+                </div>
+
+                <!-- search result accessories list -->
+                <div class="accessory-list-wrap">
+                    <div class="accessory-list col-4">
+                        <ul>
+                            <li>
+                                <div class="pic">
+                                    <a href="#"><img src="../assets/img/1.jpg" alt=""></a>
+                                </div>
+                                <div class="main">
+                                    <div class="name">XX</div>
+                                    <div class="price">999</div>
+                                    <div class="btn-area">
+                                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="pic">
+                                    <a href="#"><img src="../assets/img/2.jpg" alt=""></a>
+                                </div>
+                                <div class="main">
+                                    <div class="name">XX</div>
+                                    <div class="price">1000</div>
+                                    <div class="btn-area">
+                                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="pic">
+                                    <a href="#"><img src="../assets/img/3.jpg" alt=""></a>
+                                </div>
+                                <div class="main">
+                                    <div class="name">XX</div>
+                                    <div class="price">500</div>
+                                    <div class="btn-area">
+                                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="pic">
+                                    <a href="#"><img src="../assets/img/4.jpg" alt=""></a>
+                                </div>
+                                <div class="main">
+                                    <div class="name">XX</div>
+                                    <div class="price">2499</div>
+                                    <div class="btn-area">
+                                        <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
